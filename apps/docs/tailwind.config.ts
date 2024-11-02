643ddea110e443ad6d34634e8707df90702fd732
Luna Simons <luna@bddvlpr.com>
2024-11-02 15:53:35 +0100

    feat(docs): added daisyui

--- apps/docs/tailwind.config.ts
+++ apps/docs/tailwind.config.ts
@@ -1,11 +1,12 @@
 import type { Config } from 'tailwindcss';
 
+import typography from '@tailwindcss/typography';
+import daisyui from 'daisyui';
+
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
-
   theme: {
     extend: {}
   },
-
-  plugins: []
+  plugins: [typography, daisyui]
 } satisfies Config;
