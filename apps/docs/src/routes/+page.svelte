cfd4981b31f46323c7245c437dff0e8137f16f2f
Luna Simons <luna@bddvlpr.com>
2024-11-02 15:42:40 +0100

    chore: remove mdsvex

--- apps/docs/src/routes/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-  import { Map } from '@sveltelet/core';
-</script>
-
-<Map></Map>
--- apps/docs/svelte.config.js
+++ apps/docs/svelte.config.js
@@ -1,15 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [vitePreprocess(), mdsvex()],
+  preprocess: [vitePreprocess()],
   kit: {
     adapter: adapter()
   },
-
-  extensions: ['.svelte', '.svx']
 };
 
 export default config;
