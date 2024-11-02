db2458394ab7f8631046bee66778861e27549cf4
Luna Simons <luna@bddvlpr.com>
2024-11-02 15:30:21 +0100

    chore: added docs app struct

---
+++ apps/docs/svelte.config.js
@@ -0,0 +1,15 @@
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: [vitePreprocess(), mdsvex()],
+  kit: {
+    adapter: adapter()
+  },
+
+  extensions: ['.svelte', '.svx']
+};
+
+export default config;
